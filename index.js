c73f0f0ce1103fe1eaf50900c8ee699441d412b5
ashish <ashishvishwakarma4562@gmail.com>
2026-01-05 18:42:31 +0530

    Sd

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(
       "https://insight-admin.onrender.com",
       "https://counsillorpanel.onrender.com",
       "https://insight-counsellor-panel.onrender.com",
-      "https://insight-reception.onrender.com"
+      "https://insight-reception.onrender.com","http://counsellor.insightedu.in"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"],
